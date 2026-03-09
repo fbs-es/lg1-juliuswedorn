3011d38291b8a533f8e7f1b947ad8bf89611c66e
Julius Wedorn <wedorn.jul@fbs.ksn.at>
2026-03-09 11:16:52 +0100

    Aufgabe 10

--- Aufgabe_08/src/main/java/fbs/lg1/Aufgabe08.java
+++ Aufgabe_08/src/main/java/fbs/lg1/Aufgabe08.java
@@ -1,22 +1,20 @@
 package fbs.lg1;
 
 public class Aufgabe08 {
-
     public static void main(String[] args) {
-        check(false, 20.0, true, true);
+        System.out.println(check(false, 20.0, true, true));
     }
 
-    public static void check(boolean gefahr, double gewicht, boolean masseKorrekt, boolean zerbrechlich) {
-
+    public static String check(boolean gefahr, double gewicht, boolean masseKorrekt, boolean zerbrechlich) {
         if (gefahr) {
-            System.out.println("Annahme verweigern");
+            return "Aufnahme verweigern.";
         } else if (gewicht >= 31.5 || !masseKorrekt) {
-            System.out.println("Schalter");
+            return "Verweis an Schalter.";
         } else {
-            System.out.println("Annahme & Druck");
-
             if (zerbrechlich) {
-                System.out.println("Vermerk 'Vorsicht'");
+                return "Annahme & Druck. Vermerk 'Vorsicht'";
+            } else {
+                return "Annahme & Druck.";
             }
         }
     }
--- Aufgabe_08/src/test/java/fbs/lg1/Aufgabe08Test.java
+++ Aufgabe_08/src/test/java/fbs/lg1/Aufgabe08Test.java
@@ -1,5 +1,20 @@
 package fbs.lg1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 class Aufgabe08Test {
 
+    @Test
+    void testCheckLogic() {
+        assertEquals("Aufnahme verweigern.", Aufgabe08.check(true, 10.0, true, false));
+
+        assertEquals("Annahme & Druck. Vermerk 'Vorsicht'", Aufgabe08.check(false, 20.0, true, true));
+
+        assertEquals("Annahme & Druck.", Aufgabe08.check(false, 10.0, true, false));
+
+        assertEquals("Verweis an Schalter.", Aufgabe08.check(false, 15.0, false, false));
+
+        assertEquals("Verweis an Schalter.", Aufgabe08.check(false, 40.0, true, false));
+    }
 }
